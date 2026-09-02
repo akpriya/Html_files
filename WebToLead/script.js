33eff399793da738ece234717dc0db730e4e979c
akpriya <rpriyahspd888@gmail.com>
2026-09-02 19:12:20 +0530

    err

--- WebToLead/script.js
+++ WebToLead/script.js
@@ -1,10 +1,17 @@
-function beforeSubmit() {
-    let inputdate = document.querySelector(".inputdate");
+let captchachecked = false;
+function beforeSubmit(event) {
+    if(captchachecked){
+        let inputdate = document.querySelector(".inputdate");
     let outputdate = document.querySelector(".outputdate");
     console.log("InputDate : ", inputdate.value);
 
     //let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = inputdate.value;
+    }
+    else{
+        alert("Please check the reCAPTCHA box to submit the form.");
+        event.preventDefault();
+    }
 }
 function timestamp() {
     var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {
@@ -12,4 +19,7 @@ function timestamp() {
         elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
     }
-} setInterval(timestamp, 500); 
+} setInterval(timestamp, 500);
+function captchaSuccess() {
+    captchachecked = true;
+}
