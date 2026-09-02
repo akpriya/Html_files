055af1bf231d88a5bd425c4ec3d95f48bb5958ef
akpriya <rpriyahspd888@gmail.com>
2026-09-02 18:37:14 +0530

    smal edit

--- WebToLead/script.js
+++ WebToLead/script.js
@@ -1,4 +1,4 @@
-function beforeSubmit(){
+function beforeSubmit() {
     let inputdate = document.querySelector(".inputdate");
     let outputdate = document.querySelector(".outputdate");
     console.log("InputDate : ", inputdate.value);
@@ -6,3 +6,10 @@ function beforeSubmit(){
     //let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = inputdate.value;
 }
+function timestamp() {
+    var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+    }
+} setInterval(timestamp, 500); 
